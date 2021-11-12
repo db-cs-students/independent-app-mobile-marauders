d9a57ee551b658858efba0fa9ad8bcb3ee667e74
Logan <loganj2004@outlook.com>
2021-11-12 12:07:28 -0500

    Initial commit

---
+++ Mobile Marauders App/Mobile_Marauders_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_Marauders_AppApp.swift
+//  Mobile Marauders App
+//
+//  Created by Logan L on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_Marauders_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
