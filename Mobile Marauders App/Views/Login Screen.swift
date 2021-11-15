d3b89cc6bfc0fa14877242e81e3dc6d0422d054d
Logan <loganj2004@outlook.com>
2021-11-15 12:37:26 -0500

    Correctly Format Text Boxes

--- Mobile Marauders App/Views/Login Screen.swift
+++ Mobile Marauders App/Views/Login Screen.swift
@@ -12,50 +12,80 @@ struct Login_Screen: View {
     @State var username : String = ""
     @State var password : String = ""
     var body: some View {
-        VStack {
-            HStack {
-                Text("Username:")
-                TextField("Username", text : $username)
-                    .disableAutocorrection(true)
-            }
-            .padding()
-            HStack {
-                Text("Password:")
-                SecureField("Password", text : $password)
-                    .disableAutocorrection(true)
-            }
-            .padding()
-            Text(username)
-                .padding()
-            
-            Text(password)
-                .padding()
-            HStack {
-                Button(action: {
-                    
-                }, label: {
-                    Text("Log In")
-                        .frame(minWidth: 0, maxWidth: .infinity)
+        ZStack {
+            Color("Background").ignoresSafeArea()
+            VStack {
+                Image("Dollar")
+                    .resizable()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.black, lineWidth: 0.35))
+                    .frame(width: 200, height: 200)
+                    .padding(.bottom, 40)
+                VStack {
+                    Text("Username:")
+                    TextField("Username", text : $username)
+                        .disableAutocorrection(true)
                         .padding()
-                        .foregroundColor(.black)
-                })
-                .border(Color("Pink"), width: 2)
+                        .background(RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color.white)
+                                        .shadow(radius: 2, y:2)
+                        )
+                }
                 .padding()
+                VStack {
+                    Text("Password:")
+                    SecureField("Password", text : $password)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color.white)
+                                        .shadow(radius: 2, y:2)
+                        )
+                }
+                .padding()
+                Text(username)
+                    .padding()
                 
-                Button(action: {
-                    
-                }, label: {
-                    Text("Create Account")
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .padding()
-                        .foregroundColor(.black)
+                Text(password)
+                    .padding()
+                
+                Spacer()
+                
+                HStack {
+                    Button(action: {
                         
-                })
-                .background(Color("Pink"))
-                .padding()
+                    }, label: {
+                        Text("Log In")
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .padding()
+                            .foregroundColor(.black)
+                    })
+                    .background(RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color.white)
+                                    .shadow(radius: 2, y:2)
+                    )
+                    .padding()
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Create Account")
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .padding()
+                            .foregroundColor(.black)
+                        
+                    })
+                    .background(RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color("Pink"))
+                                    .shadow(radius: 2, y:2)
+                    )
+                    .padding()
+                    
+                }
+                .padding(.bottom, 40)
             }
         }
-        .background(Color("Background"))
+        
     }
 }
 
