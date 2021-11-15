505b5dfd3a4f3e2420bb55dfa2f53dab11cac3bd
Grant M <grantmcdowell2004@gmail.com>
2021-11-15 13:02:10 -0500

    work on progress bars

---
+++ Mobile Marauders App/Views/ProgressBar.swift
@@ -0,0 +1,31 @@
+//
+//  ProgressBar.swift
+//  Mobile Marauders App
+//
+//  Created by Grant M on 11/15/21.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var body: some View {
+        ZStack {
+        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/15.0/*@END_MENU_TOKEN@*/)
+            .frame(width: 250, height: 50)
+            .shadow(radius: 3, x: 0, y: 3)
+            
+        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/15.0/*@END_MENU_TOKEN@*/)
+            .frame(width: 200, height: 50, alignment: .trailing)
+            .shadow(radius: 3, x: 0, y: 3)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+
+        }
+            
+    }
+}
+
+struct ProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressBar()
+    }
+}
