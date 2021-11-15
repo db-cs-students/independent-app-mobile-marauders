c1dd973868c7a9bf335446432108a7b90eeb7dcc
Logan <loganj2004@outlook.com>
2021-11-15 12:47:12 -0500

    Initialize New User

---
+++ Mobile Marauders App/Views/New User.swift
@@ -0,0 +1,20 @@
+//
+//  New User.swift
+//  Mobile Marauders App
+//
+//  Created by Logan L on 11/15/21.
+//
+
+import SwiftUI
+
+struct New_User: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct New_User_Previews: PreviewProvider {
+    static var previews: some View {
+        New_User()
+    }
+}
