c183a6f48a079fdb88f6a250208ce5624fcb7736
Ian M <kidenderman04@gmail.com>
2021-11-17 12:26:31 -0500

    BUTTON

---
+++ Mobile Marauders App/Views/GButton.swift
@@ -0,0 +1,39 @@
+//
+//  SwiftUIView.swift
+//  Mobile Marauders App
+//
+//  Created by Ian M on 11/12/21.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    @State private var nav = false
+    var body: some View {
+            NavigationView {
+                    Button(action: {nav.toggle()}) {
+                        ZStack {
+                            Color.init(red: 255.0 / 255.0, green: 174.0 / 255.0, blue: 174.0 / 255.0)
+                            Text("Button")
+                                .font(.title)
+                                .foregroundColor(Color.black)
+                        }
+                    }
+                    .frame(width: 270, height: 50)
+                    .cornerRadius(50.0)
+                    .shadow(radius: 8)
+            
+                if nav {
+                    NavigationLink(destination: Text("Second View")) {
+                    Text("Navigation")
+                    }
+                }
+            }
+        }
+    }
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
+
