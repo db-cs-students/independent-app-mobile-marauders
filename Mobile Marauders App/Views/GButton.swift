cd243e2ec42f9d9c0b353a1c3559a0c970acb7e6
Ian M <kidenderman04@gmail.com>
2021-11-17 12:43:48 -0500

    still fixn

--- Mobile Marauders App/Views/GButton.swift
+++ Mobile Marauders App/Views/GButton.swift
@@ -1,20 +1,37 @@
 //
-//  GButton.swift
+//  SwiftUIView.swift
 //  Mobile Marauders App
 //
-//  Created by Ian M on 11/17/21.
+//  Created by Ian M on 11/12/21.
 //
-
 import SwiftUI
 
-struct GButton: View {
+struct SwiftUIView: View {
+    @State private var nav = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            NavigationView {
+                    Button(action: {nav.toggle()}) {
+                        ZStack {
+                            Color.init(red: 255.0 / 255.0, green: 174.0 / 255.0, blue: 174.0 / 255.0)
+                            Text("Button")
+                                .font(.title)
+                                .foregroundColor(Color.black)
+                        }
+                    }
+                    .frame(width: 270, height: 50)
+                    .cornerRadius(50.0)
+                    .shadow(radius: 8)
+            
+                if nav {
+                    NavigationLink(destination: Text("Second View")) {
+                    Text("Navigation")
+                    }
+                }
+            }
+        }
     }
-}
-
-struct GButton_Previews: PreviewProvider {
+struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        GButton()
+        SwiftUIView()
     }
 }
