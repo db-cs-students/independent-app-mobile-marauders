4dabb8a031b38fd08d7fe62bccaba0fd8d8beaf6
Ian M <kidenderman04@gmail.com>
2021-11-17 12:42:57 -0500

    Fixin 't

---
+++ Mobile Marauders App/Views/GButton.swift
@@ -0,0 +1,20 @@
+//
+//  GButton.swift
+//  Mobile Marauders App
+//
+//  Created by Ian M on 11/17/21.
+//
+
+import SwiftUI
+
+struct GButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GButton_Previews: PreviewProvider {
+    static var previews: some View {
+        GButton()
+    }
+}
