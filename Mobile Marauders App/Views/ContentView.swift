ce9c653dc235c599e0c2e437258fe4f60e2d0fe4
Grant M <grantmcdowell2004@gmail.com>
2021-11-23 13:00:56 -0500

    Work on advice tab

--- Mobile Marauders App/Views/ContentView.swift
+++ Mobile Marauders App/Views/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+            VStack {
+                Text("Featured Articles :")
+                Link("8 Financial Tips for Young Adults", destination: URL(string: "https://www.investopedia.com/articles/younginvestors/08/eight-tips.asp")!)
+                    .padding()
+                Link("50 Personal Finance Tips That Will Change the Way You Think About Money", destination: URL(string: "https://www.themuse.com/advice/50-personal-finance-tips-that-will-change-the-way-you-think-about-money")!)
+                    .padding()
+                Link("How to Start Investing in Stocks: A Beginner's Guide", destination: URL(string: "https://www.investopedia.com/articles/basics/06/invest1000.asp")!)
+                    .padding()
+                Spacer()
+            }
             .padding()
+            VStack {
+                Text("Challenges :")
+                    .padding()
+                VStack(alignment: .leading) {
+                    Text("No Monthly Budget Overdraft :")
+                        .padding(.bottom)
+                    Text("8 Days Remaining")
+                        .padding(.bottom)
+                    Text("Log-in Streak: 20 Days")
+                    Spacer()
+                }
+            }
+            .padding()
+        }
     }
 }
 
