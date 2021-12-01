0110bfbc4fd37b4fc8355a40aa7b759fb810f3af
Ian M <kidenderman04@gmail.com>
2021-12-01 11:57:02 -0500

    We can go into WebViews AND exit now!

--- Mobile Marauders App/Views/AdviceView.swift
+++ Mobile Marauders App/Views/AdviceView.swift
@@ -34,33 +34,29 @@ struct Article: Hashable, Identifiable {
 }
 
 struct AdviceView: View {
-    @State var isShowingWebView: Bool = false
     @Environment(\.openURL) var openURL
     
     let articles = [
         Article("The 50-20-30 rule", url: "https://n26.com/en-eu/blog/50-30-20-rule"),
-        Article("Ten Tips", url: "https://www.strawberryinvest.com/knowledge-hub/top-10-tips-for-first-time-investors/")
+        Article("Ten Tips", url: "https://www.strawberryinvest.com/knowledge-hub/top-10-tips-for-first-time-investors/"),
+        Article("How to buy a car", url: "https://www.ramseysolutions.com/debt/the-truth-about-car-payments")
     ]
     
     var body: some View {
         VStack {
             Text("Articles on Budgeting!")
-                .padding()
-            
+            NavigationView {
             List {
                 ForEach(articles, id: \.self) { article in
-                    Button(action: {
-                              isShowingWebView = true
-                          })
-                          {
-                        Text("\(article.text)")
-                          }
-                          .sheet(isPresented: $isShowingWebView) {
-                            WebView(request: URLRequest(url: URL(string: article.url)!))
-                          }
-                    .padding(.bottom, 10.0)
+                    NavigationLink(
+                        destination: WebView(request: URLRequest(url: URL(string: article.url)!)), label: {
+                            Text("\(article.text)")
+                        })
+                    }
                 }
             }
+            .frame(width: 300, height: 250)
+            Text("Dailey Challenges")
         }
     }
 }
@@ -68,11 +64,5 @@ struct AdviceView: View {
 struct AdviceView_Previews: PreviewProvider {
     static var previews: some View {
         AdviceView()
-    }
-}
-
-struct WebView_Previews : PreviewProvider {
-    static var previews: some View {
-        WebView(request: URLRequest(url: URL(string: "https://google.com")!))
     }
 }
