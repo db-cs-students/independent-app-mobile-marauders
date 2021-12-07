c147828b6f4e8b52da1a7a55e445bf0fb9fc4031
Logan <loganj2004@outlook.com>
2021-12-07 12:46:50 -0500

    Fix Merge Commit

--- Mobile Marauders App/Data/Structs.swift
+++ Mobile Marauders App/Data/Structs.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Data: ObservableObject {
     @Published var payments: [Payment] = [Payment(name: "Car Payment", amount: 200.00, type: Type.Car), Payment(name: "House Payment", amount: 600.00, type: Type.House), Payment(name:"Phone Payment", amount: 125.00, type: Type.Phone)]
-    @Published var budgets : [Budget] = []
+    @Published var budgets : [Budget] = [Budget(name: "Subscriptions", amount: 50.00, repeatDate: Repeat.Monthly, daysLeft: 30, pace: false)]
     @Published var monthlyIncome : Double = 2500.00
     @Published var monthlyExpenses : Double = 1234.00
 }
@@ -39,6 +39,8 @@ struct Budget: Hashable, Identifiable {
     var name: String
     var amount : Double
     var repeatDate : Repeat
+    var daysLeft : Int
+    var pace : Bool
 }
 
 struct Payment: Hashable, Identifiable {
