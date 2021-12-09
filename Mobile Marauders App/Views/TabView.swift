67d56dd15be95ef4fe48aa748d3b3b3805acf2d0
Ian M <kidenderman04@gmail.com>
2021-12-09 11:50:47 -0500

    tabview started

---
+++ Mobile Marauders App/Views/TabView.swift
@@ -0,0 +1,22 @@
+//
+//  TabView.swift
+//  Mobile Marauders App
+//
+//  Created by Ian M on 12/9/21.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        ZStack {
+            Color.init(red: 255.0 / 255.0, green: 174.0 / 255.0, blue: 174.0 / 255.0)
+        }
+    }
+}
+
+struct TabView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabView()
+    }
+}
